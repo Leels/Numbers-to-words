c61359ecc0fe0571dfe81141ce57035711487c66
Student <example@example.com>
2019-12-10 13:45:29 -0800

    convert single digit number to numeronym. passed spec.

--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -3,8 +3,13 @@ require ('pry')
 require ('number_to_word')
 
 describe ('alpha_num') do
+  # it("converts a number to a string") do
+  #   numeronym = Numeronym.new(1)
+  #   expect(numeronym.convert()).to(eq(["1"]))
+  # end
+
   it("converts a number to a string") do
     numeronym = Numeronym.new(1)
-    expect(numeronym.convert()).to(eq(["1"]))
+    expect(numeronym.convert()).to(eq("one"))
   end
 end
